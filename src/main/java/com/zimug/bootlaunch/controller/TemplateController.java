176f24388378bc40f1e39c378a555ec3bb9904d5
wuhong <wuhong@cloud-star.com.cn>
2020-02-13 22:16:47 +0800

    feat(*): 整合freemarker

--- src/main/java/com/zimug/bootlaunch/controller/TemplateController.java
+++ src/main/java/com/zimug/bootlaunch/controller/TemplateController.java
@@ -1,5 +1,8 @@
 package com.zimug.bootlaunch.controller;
 
+import com.zimug.bootlaunch.model.ArticleVO;
+import com.zimug.bootlaunch.service.ArticleRestService;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Resource;
 
 @Controller
 @RequestMapping("/template")
@@ -20,4 +26,24 @@ public class TemplateController {
          return "thymeleaftemp";
     }
 
+
+    @Resource(name="articleMybatisRestServiceImpl")
+    ArticleRestService articleRestService;
+
+
+    //@ModelView
+    @GetMapping("/freemarker")
+    public String index(Model model) {
+        List<ArticleVO> articles = articleRestService.getAll();
+        ArticleVO articleVO = new ArticleVO();
+        articleVO.setAuthor("张三");
+        articleVO.setTitle("redis");
+        articleVO.setContent("哈哈哈哈哈");
+        articles.add(articleVO);
+
+        model.addAttribute("articles", articles);
+
+        //模版名称，实际的目录为：resources/templates/fremarkertemp.html
+        return "fremarkertemp";
+    }
 }

6cdcfc62ce128b3baec79c5d8f220180f9a00a2d
wuhong <444089024@qq.com>
2021-02-13 11:01:40 +0800

    feat(*): rockmq测试

---
+++ src/main/java/com/zimug/bootlaunch/mq/rocketmq/test/OnewayProducer.java
@@ -0,0 +1,25 @@
+package com.zimug.bootlaunch.mq.rocketmq.test;
+
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+public class OnewayProducer {
+    public static void main(String[] args) throws Exception {
+        // 实例化消息生产者Producer
+        DefaultMQProducer producer = new DefaultMQProducer("please_rename_unique_group_name");
+        // 设置NameServer的地址
+        producer.setNamesrvAddr("192.168.93.100:9876");
+        // 启动Producer实例
+        producer.start();
+        for (int i = 0; i < 1; i++) {
+            // 创建消息，并指定Topic，Tag和消息体
+            byte[] bytes = ("Hello RocketMQ " + i).getBytes(RemotingHelper.DEFAULT_CHARSET);
+            Message msg = new Message("TopicTest", "TagA", bytes);
+            // 发送单向消息，没有任何返回结果
+            producer.sendOneway(msg);
+        }
+        // 如果不再发送消息，关闭Producer实例。
+        producer.shutdown();
+    }
+}
